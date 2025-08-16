d4ebd4e8967b8f9d415834ffd7117a4ebefd83b9
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-08-16 22:20:43 +0200

    set color with action buttons

--- src/0.init.lua
+++ src/0.init.lua
@@ -2,15 +2,19 @@
 
 x=64
 y=64
+c=127
 
 function _update()
  if (btn(0)) then x-=1 end
  if (btn(1)) then x+=1 end
  if (btn(2)) then y-=1 end
  if (btn(3)) then y+=1 end
+ if (btn(4)) then c=127 end
+ if (btn(5)) then c=64 end
 end
 
 function _draw()
  rectfill(0,0,127,127,5)
- circfill(x,y,7,14)
+ circfill(x,y,7,c)
 end
+
