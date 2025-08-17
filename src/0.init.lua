609a09b927af8189e4385b3ab8b61f5c674834f5
Lyn Nagara <lynnagara@users.noreply.github.com>
2025-08-17 13:18:23 -0700

    detect edge of screen

--- src/0.init.lua
+++ src/0.init.lua
@@ -5,16 +5,21 @@ y=64
 c=127
 
 function _update()
- if (btn(0)) then x-=1 end
- if (btn(1)) then x+=1 end
- if (btn(2)) then y-=1 end
- if (btn(3)) then y+=1 end
- if (btn(4)) then c=127 end
- if (btn(5)) then c=64 end
+    if (btn(0)) then x-=1 end
+    if (btn(1)) then x+=1 end
+    if (btn(2)) then y-=1 end
+    if (btn(3)) then y+=1 end
+    if (btn(4)) then y-=4 end
+    if (btn(5)) then c=64 end
+
+    -- edge collision
+    x = mid(0, x, 127)
+    y = mid(0, y, 127)
+
 end
 
 function _draw()
- rectfill(0,0,127,127,5)
- spr(1,x,y)
+    rectfill(0,0,127,127,5)
+    spr(1,x,y)
 end
 
