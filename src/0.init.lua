233ab797a85a1500fe65ef1b65c537d453e419fa
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-08-18 14:17:21 +0200

    initial game update

--- src/0.init.lua
+++ src/0.init.lua
@@ -1,8 +1,37 @@
 --hackweek-pico8-game
+-- vim: sw=2 ts=2 et
 
 x=64
-y=64
+y=127 - 8
 c=127
+clock=0
+
+-- const
+num_enemies = 8
+row_height = 12
+gap_width = 10
+bg_color = 7
+
+rows = {}
+enemies = {}
+
+function sign(x)
+  -- can't use int division because of femto8
+  return x / abs(x)
+end
+
+function _init()
+  for i = 1,num_enemies do
+    rows[i] = {}
+    prev = 0
+    for j = 1,rnd(3) + 1 do
+      prev = rnd(128 - gap_width - prev)
+      rows[i][j] = prev
+    end
+
+    enemies[i] = { x_pos = rnd(127 - gap_width), dir = 1 }
+  end
+end
 
 function _update()
     if (btn(0)) then x-=1 end
@@ -23,3 +52,9 @@ function _draw()
     spr(1,x,y)
 end
 
+  for y,data in pairs(enemies) do
+    spr(2,data.x_pos, y * row_height + 4)
+  end
+
+  spr(1,x,y)
+end
